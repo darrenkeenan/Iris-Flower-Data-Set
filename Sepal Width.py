f222dd2a69da34fddd0e6c29000b58b2662d7c75
Darren Keenan <36712971+darrenkeenan@users.noreply.github.com>
2018-04-29 14:20:54 +0100

    Update Sepal Width.py

--- Sepal Width.py
+++ Sepal Width.py
@@ -1,25 +1,34 @@
-columnname = "Sepal Width", "name"
-print(columnname)
+# Calculating min & max sepal width per species of iris data set
+# Darren Keenan - finalized 2018-04-29
 
-with open("data/iris.csv") as iris:
+columnname = "Sepal Width", "Species/Class"                       # naming two columns
+print(columnname)                                                 # printing named columns    
+
+with open("data/iris.csv") as iris:                               # opening file iris.csv saved on computer
     for cmandname in iris:
-        i = cmandname.split(",")
-        print(i[3],i[4]) 
+        i = cmandname.split(",")                                  # aligning data
+        print(i[3],i[4])                                          # listing column 4 in file, sepal width & column 5, spcies/class of iri
 
 listset = (0.2,0.2,0.2,0.2,0.2,0.4,0.3,0.2,0.2,0.1,0.2,0.2,0.1,0.1,0.2,0.4,0.4,0.3,0.3,0.3,0.2,0.4,0.2,0.5,0.2,0.2,0.4,0.2,0.2,0.2,0.2,0.4,0.1,0.2,0.1,0.2,0.2,0.1,0.2,0.2,0.3,0.3,0.2,0.6,0.4,0.3,0.2,0.2,0.2,0.2)
-print("Min Iris-Setosa Sepal Width is:", min(listset))
+print("Min Iris-Setosa Sepal Width is:", min(listset))            # printing minimum value of listset
+# Min Iris-Setosa Sepal Width is: 0.1
 
 listset = (0.2,0.2,0.2,0.2,0.2,0.4,0.3,0.2,0.2,0.1,0.2,0.2,0.1,0.1,0.2,0.4,0.4,0.3,0.3,0.3,0.2,0.4,0.2,0.5,0.2,0.2,0.4,0.2,0.2,0.2,0.2,0.4,0.1,0.2,0.1,0.2,0.2,0.1,0.2,0.2,0.3,0.3,0.2,0.6,0.4,0.3,0.2,0.2,0.2,0.2)
-print("Max Iris-Setosa Sepal Width is:", max(listset))
+print("Max Iris-Setosa Sepal Width is:", max(listset))            # printing maximum value of listset
+# Max Iris-Setosa Sepal Width is: 0.6
 
 listset = (1.4,1.5,1.5,1.3,1.5,1.3,1.6,1,1.3,1.4,1,1.5,1,1.4,1.3,1.4,1.5,1,1.5,1.1,1.8,1.3,1.5,1.2,1.3,1.4,1.4,1.7,1.5,1,1.1,1,1.2,1.6,1.5,1.6,1.5,1.3,1.3,1.3,1.2,1.4,1.2,1,1.3,1.2,1.3,1.3,1.1,1.3)
-print("Min Iris-Versicolor Sepal Width is:", min(listset))
+print("Min Iris-Versicolor Sepal Width is:", min(listset))        # printing minimum value of listset
+# Min Iris-Versicolor Sepal Width is: 1
 
 listset = (1.4,1.5,1.5,1.3,1.5,1.3,1.6,1,1.3,1.4,1,1.5,1,1.4,1.3,1.4,1.5,1,1.5,1.1,1.8,1.3,1.5,1.2,1.3,1.4,1.4,1.7,1.5,1,1.1,1,1.2,1.6,1.5,1.6,1.5,1.3,1.3,1.3,1.2,1.4,1.2,1,1.3,1.2,1.3,1.3,1.1,1.3)
-print("Max Iris-Versicolor Sepal Width is:", max(listset))
+print("Max Iris-Versicolor Sepal Width is:", max(listset))        # printing maximum value of listset
+# Max Iris-Versicolor Sepal Width is: 1.8
 
 listset = (2.5,1.9,2.1,1.8,2.2,2.1,1.7,1.8,1.8,2.5,2,1.9,2.1,2,2.4,2.3,1.8,2.2,2.3,1.5,2.3,2,2,1.8,2.1,1.8,1.8,1.8,2.1,1.6,1.9,2,2.2,1.5,1.4,2.3,2.4,1.8,1.8,2.1,2.4,2.3,1.9,2.3,2.5,2.3,1.9,2,2.3,1.8)
-print("Min Iris-Virginica Sepal Width is:", min(listset))
+print("Min Iris-Virginica Sepal Width is:", min(listset))         # printing minimum value of listset
+# Min Iris-Virginica Sepal Width is: 1.4
 
 listset = (2.5,1.9,2.1,1.8,2.2,2.1,1.7,1.8,1.8,2.5,2,1.9,2.1,2,2.4,2.3,1.8,2.2,2.3,1.5,2.3,2,2,1.8,2.1,1.8,1.8,1.8,2.1,1.6,1.9,2,2.2,1.5,1.4,2.3,2.4,1.8,1.8,2.1,2.4,2.3,1.9,2.3,2.5,2.3,1.9,2,2.3,1.8)
-print("Max Iris-Virginica Sepal Width is:", max(listset))
+print("Max Iris-Virginica Sepal Width is:", max(listset))         # printing maximum value of listset
+# Max Iris-Virginica Sepal Width is: 2.5
